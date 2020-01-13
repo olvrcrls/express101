7bd1f4ddc933ca789905cefa78c9845808a81325
Oli <dev.olivercarlos@gmail.com>
2020-01-13 21:21:58 +0800

    Use json method on response parameter on an API endpoint.

--- helmetAndOthers.js
+++ helmetAndOthers.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const helmet = require('helmet');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.json());
+app.use(helmet());
 // This is important because it will parse the data body.
 app.use(express.urlencoded({extended: false}));
 
 app.post('/ajax', (req, res) => {
     // console.log(req);
     console.log(req.body);
-    res.send('POST test.');
+    
+    res.json('POST test.');
 });
 
 
